eb2606834c0d149e1833f2848880c4d5daafefeb
TAN.TON <nhattan2007@gmail.com>
2020-11-09 15:14:57 +0700

    C2 - [Thuc hanh] Luyen tap voi cau truc if/else

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,26 @@
-let year = parseInt(prompt("Nhập năm: "));
-let isLeapYear = false;
-if (year % 4 == 0) {
-      if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                  isLeapYear = true;
-            }
-      } else {
-            isLeapYear = true;
-      }
-}
+//[Thuc hanh] Kiem tra nam nhuan
+// let year = parseInt(prompt("Nhập năm: "));
+// let isLeapYear = false;
+// if (year % 4 == 0) {
+//       if (year % 100 == 0) {
+//             if (year % 400 == 0) {
+//                   isLeapYear = true;
+//             }
+//       } else {
+//             isLeapYear = true;
+//       }
+// }
 
-if (isLeapYear) {
-      alert(`Năm ${year} là năm nhuận`);
+// if (isLeapYear) {
+//       alert(`Năm ${year} là năm nhuận`);
+// } else {
+//       alert(`Năm ${year} không phải là năm nhuận `);
+// }
+
+//[Thuc hanh] Luyen tap voi cau truc if/else
+let theAnswer = prompt("What is the “official” name of JavaScript?");
+if (theAnswer == "ECMAScript") {
+      alert("Right!");
 } else {
-      alert(`Năm ${year} không phải là năm nhuận `);
+      alert("Didn’t know? ECMAScript!");
 }
-
